de1107336f0bed5c3936d021aa3258888e2729c4
Joao Pedro <jguedespresto@gmail.com>
2017-12-15 09:16:01 -0200

    Construcao Classe Producao

--- ProducaoSerie/src/model/Producao.java
+++ ProducaoSerie/src/model/Producao.java
@@ -5,10 +5,45 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author JohnPeter
  */
 public class Producao {
     
+    private double verba;
+    private ArrayList<Cena> cenas;
+    private ArrayList<Registro> registros;
+
+    public Producao(double verba) {
+        registros = new ArrayList<>();
+        cenas = new ArrayList<>();
+        this.verba = verba;        
+    }
+
+    public double getVerba() {
+        return verba;
+    }
+
+    public void setVerba(double verba) {
+        this.verba = verba;
+    }
+
+    public ArrayList<Cena> getCenas() {
+        return cenas;
+    }
+
+    public void setCenas(ArrayList<Cena> cenas) {
+        this.cenas = cenas;
+    }
+
+    public ArrayList<Registro> getRegistros() {
+        return registros;
+    }
+
+    public void setRegistros(ArrayList<Registro> registros) {
+        this.registros = registros;
+    }   
 }
