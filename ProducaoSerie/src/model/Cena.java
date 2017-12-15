a1d6a829850e6e1318c0311fcd3bfcc286740567
Joao Pedro <jguedespresto@gmail.com>
2017-12-15 09:24:11 -0200

    Construcao Classe Cena

--- ProducaoSerie/src/model/Cena.java
+++ ProducaoSerie/src/model/Cena.java
@@ -5,10 +5,68 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author JohnPeter
  */
 public class Cena {
     
+    private int duracao, id;
+    private ArrayList<Fita> fitas;
+    private ArrayList<Tomada> tomadas;
+    private Tomada tomadaAprovada;
+
+    public Cena(int duracao, int id) {
+        fitas = new ArrayList<>();
+        tomadas = new ArrayList<>();
+        this.duracao = duracao;
+        this.id = id;
+        this.tomadaAprovada = tomadaAprovada;
+    }
+
+    public Cena() {
+        this(0, 0);
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Fita> getFitas() {
+        return fitas;
+    }
+
+    public void setFitas(ArrayList<Fita> fitas) {
+        this.fitas = fitas;
+    }
+
+    public ArrayList<Tomada> getTomadas() {
+        return tomadas;
+    }
+
+    public void setTomadas(ArrayList<Tomada> tomadas) {
+        this.tomadas = tomadas;
+    }
+
+    public Tomada getTomadaAprovada() {
+        return tomadaAprovada;
+    }
+
+    public void setTomadaAprovada(Tomada tomadaAprovada) {
+        this.tomadaAprovada = tomadaAprovada;
+    }   
 }
